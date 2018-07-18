bd0a52f2c0e9b361d4dc2180114ac6a46ffccda8
chelly <591934196@qq.com>
2018-07-18 19:00:10 +0800

    Add two pages

---
+++ pages/logs/logs.js
@@ -0,0 +1,14 @@
+//logs.js
+Page({
+  data: {
+    imgUrls: [
+      '/images/iqiyi.png',
+      '/images/vr.png',
+      '/images/wx.png'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+  },
+})
